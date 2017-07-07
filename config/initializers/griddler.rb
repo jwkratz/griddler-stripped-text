57899228b648f581b3f7331fbd2fb0aab0714167
Jeremy Kratz <jwkratz@gmail.com>
2017-07-06 21:30:42 -0500

    Monkey patched Griddler to account for Mailgun stripped-text param

--- config/initializers/griddler.rb
+++ config/initializers/griddler.rb
@@ -1,3 +1,31 @@
 Griddler.configure do |config|
   config.email_service = :mailgun
 end
+
+# Add Mailgun stripped-text to normalized params
+module GriddlerMailgunAdapterExtensions
+  def normalize_params
+    normalized_params = super
+    normalized_params[:stripped_text] = params['stripped-text']
+    normalized_params
+  end
+end
+
+# Prepend custom extensions
+Griddler::Mailgun::Adapter.class_eval do
+  prepend GriddlerMailgunAdapterExtensions
+end
+
+# Assign Mailgun stripped-text to attribute
+module GriddlerEmailExtensions
+  def initialize(params)
+    super
+    @stripped_text = params[:stripped_text]
+  end
+end
+
+# Add attribute for Mailgun stripped-text and prepend custom extensions
+Griddler::Email.class_eval do
+  attr_reader :stripped_text
+  prepend GriddlerEmailExtensions
+end
